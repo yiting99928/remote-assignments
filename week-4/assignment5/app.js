61d8a592663add53e1c3ff05706dad43f4857fbf
yiting <yumy19990628@gmail.com>
2023-02-03 12:26:53 +0800

    revise Fn name #6 14 21

--- week-4/assignment5/app.js
+++ week-4/assignment5/app.js
@@ -3,7 +3,7 @@ const { useState } = React //解構
 const App = () => {
     const [num, setNum] = useState([0,0,0]);
 
-    const AddNum = (i) => {
+    const addNum = (i) => {
         setNum(prevNum => {
             const newNum = [...prevNum];
             newNum[i] += 1;
@@ -11,31 +11,31 @@ const App = () => {
         });
     }
     
-    const AddAll = () => {
+    const addAll = () => {
         setNum(prevNum => {
             const newNum = prevNum.map(num => num + 1);
             return newNum;
         });
     }
 
-    const AddCounter = () => {
+    const addCounter = () => {
         setNum([...num,0])
     }
 
     const counter = num.map((num ,i)=>{
         return (
             <div key={i} style={{display:"flex"}}>
-                <button type="button" className="btn btn-outline-primary m-1" onClick={() => AddNum(i)}>+1</button>
+                <button type="button" className="btn btn-outline-primary m-1" onClick={() => addNum(i)}>+1</button>
                 <p className="m-2" >{num}</p>
             </div>
         )
     });
     return (<>
-        <button className="btn btn-primary" type="button" onClick={AddAll}>All + 1</button>        
+        <button className="btn btn-primary" type="button" onClick={addAll}>All + 1</button>        
         <div className="m-2" >
             {counter}
         </div>
-        <button className="btn btn-primary" type="button" onClick={AddCounter}>Add a Counter</button>
+        <button className="btn btn-primary" type="button" onClick={addCounter}>Add a Counter</button>
         </>
     )
 }
